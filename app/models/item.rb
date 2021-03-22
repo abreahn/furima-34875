85ae9ca522d11301b8de44a8b76be886bffa3f21
abreahn <iriss146969@yahoo.co.jp>
2021-03-22 18:28:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :delivery_area
   belongs_to :delivery_charge
   belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :condition
+    validates :delivery_area
+    validates :delivery_charge
+    validates :delivery_day
+  end
 end
