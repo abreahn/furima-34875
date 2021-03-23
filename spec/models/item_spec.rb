7f2e9e7c15b22041aebfc0c99283a54b6b6064ed
abreahn <iriss146969@yahoo.co.jp>
2021-03-23 20:32:29 +0900

    バリデーション修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,31 +31,31 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーの情報が無いと保存できないこと' do
       @item.category_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Category is not a number')
+      expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
     it '商品状態についての情報が無いと保存できないこと' do
       @item.condition_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Condition is not a number')
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
     end
 
     it '配送料の負担についての情報が無いと保存できないこと' do
       @item.delivery_charge_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery charge is not a number')
+      expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
     end
 
     it '発送元の地域についての情報が無いと保存できないこと' do
       @item.delivery_area_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery area is not a number')
+      expect(@item.errors.full_messages).to include("Delivery area can't be blank")
     end
 
     it '発送までの日数についての情報が無いと保存できないこと' do
       @item.delivery_day_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery day is not a number')
+      expect(@item.errors.full_messages).to include("Delivery day can't be blank")
     end
 
     it '販売価格についての情報が無いと保存できないこと' do
