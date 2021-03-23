05555888e707c1e975c47ca911ab24c12e793a98
abreahn <iriss146969@yahoo.co.jp>
2021-03-23 19:59:07 +0900

    保存できない場合のコントローラー設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,8 @@ class Item < ApplicationRecord
     validates :price
     validates :image
   end
-  with_options numericality: { other_than: 1 } do
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
     validates :delivery_area_id
