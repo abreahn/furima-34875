c4c832199f97a67b47ba7b4928aa0d162690626d
abreahn <iriss146969@yahoo.co.jp>
2021-03-25 12:21:05 +0900

    rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user.id
   end
 end
