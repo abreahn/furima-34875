6e32ff67d3c643f12e69ebe223a4eb6d28762fcd
abreahn <iriss146969@yahoo.co.jp>
2021-03-27 18:45:26 +0900

    エラーメッセージの表示

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -6,10 +6,10 @@ class PurchaseShipping
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :delivery_area_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :delivery_area_id, numericality: {other_than: 1, message: "select"}
     validates :city
     validates :addresses
-    validates :phone_number, format: {with: /\A\d{1,11}\z/}
+    validates :phone_number, format: {with: /\A\d{1,11}\z/, message: "input only number"}
   end
     
   def save
