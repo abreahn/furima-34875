6d23b929f652846082d72edd075ed45757c10c10
abreahn <iriss146969@yahoo.co.jp>
2021-03-27 18:31:20 +0900

    コントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,23 @@
 class PurchasesController < ApplicationController
-def index
-end
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_shipping = PurchaseShipping.new
+  end
+
+  def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping).permit(:postal_code, :delivery_area_id, :city, :addresses, :building, :phone_number, :number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
