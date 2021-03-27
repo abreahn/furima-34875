a5f54efeb73d1e53bc962d12de5e32d1e7d98389
abreahn <iriss146969@yahoo.co.jp>
2021-03-27 18:32:06 +0900

    Formオブジェクトの設定

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,20 +1,19 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :delivery_area, :city, :addresses, :building, :phone_number, :purchase_id
+  attr_accessor :user_id, :item_id, :postal_code, :delivery_area_id, :city, :addresses, :building, :phone_number, :purchase_id, :image, :number, :exp_month, :exp_year, :cvc
 
-  with_options presence: ture do
+  with_options presence: true do
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :delivery_area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :addresses
-    validates :phone_number, numericality: {only_integer: true, length: { in: ..11}}
-    validates :purchase_id
+    validates :phone_number, format: {with: /\A\d{1,11}\z/}
   end
     
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, addresses: addresses, phone_number: phone_number, purchase_id: purchase_id)
+    Shipping.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, addresses: addresses, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
